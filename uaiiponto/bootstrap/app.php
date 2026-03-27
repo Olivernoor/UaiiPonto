3df9a9be92294b1e4a97314729faadace1847407
oliver <olivercaua0@gmail.com>
2026-03-27 17:17:42 -0300

    Implementação do backend de cadastro

--- uaiiponto/bootstrap/app.php
+++ uaiiponto/bootstrap/app.php
@@ -11,7 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'login',
+            'register',
+            'logout',
+            'me',
+            'users/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
